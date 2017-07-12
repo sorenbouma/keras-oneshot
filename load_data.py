94d372fc936a470c3d3be1a1f356cdbaa82845c5
soren <sbou093@aucklanduni.ac.nz>
2017-07-12 22:58:54 +1200

    Improved loading script to make it easier for others to use

--- load_data.py
+++ load_data.py
@@ -4,42 +4,54 @@ from scipy.misc import imread
 import dill as pickle
 import os
 import matplotlib.pyplot as plt
-basepath = '/home/soren/Documents/oneshot/omniglot/python/images_background/'
-valpath = '/home/soren/Documents/oneshot/omniglot/python/images_evaluation/'
-lang_dict = {}
-def loadimgs(path,n=0):
-	X=[]
-	y = []
-	cat_dict = {}
-	curr_y = n
-        #I want to make dict {"lang":(start,end)} for every language
-	for alphabet in os.listdir(path):
-                lang_dict[alphabet] = [curr_y,None]
-		for letter in os.listdir(path+alphabet):
-			cat_dict[curr_y] = (alphabet, letter)
-			currpath = path + alphabet + '/'
-			category_images=[]
-			for example in os.listdir(currpath+letter):
-				image = imread(currpath+letter+'/'+example)
-				category_images.append(image)
-				y.append(curr_y)
-			X.append(np.stack(category_images))
-			curr_y += 1
-                lang_dict[alphabet][1] = curr_y - 1
-	X=np.stack(X)
-	y=np.vstack(y)
-#	print(cat_dict)
-	return X,y,lang_dict
+import argparse
+"""Script to preprocess the omniglot dataset and pickle it into an array that's easy
+    to index my character type"""
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--path",help="Path where you've downloaded the omniglot images")
+args = parser.parse_args()
+data_path = args.path
+basepath = os.path.join(data_path,'python/images_background')
+valpath = os.path.join(data_path,'python/images_evaluation')
+
+def loadimgs(path,n=0):
+    X=[]
+    y = []
+    cat_dict = {}
+    lang_dict = {}
+    curr_y = n
+    #we load every alphabet seperately so we can isolate them later
+    for alphabet in os.listdir(path):
+        print("loading alphabet: " + alphabet)
+        lang_dict[alphabet] = [curr_y,None]
+        alphabet_path = os.path.join(path,alphabet)
+        #every letter/category has it's own column in the array, so  load seperately
+        for letter in os.listdir(alphabet_path):
+            cat_dict[curr_y] = (alphabet, letter)
+            category_images=[]
+            letter_path = os.path.join(alphabet_path, letter)
+            for filename in os.listdir(letter_path):
+                image_path = os.path.join(letter_path, filename)
+                image = imread(image_path)
+                category_images.append(image)
+                y.append(curr_y)
+            try:
+                X.append(np.stack(category_images))
+            #edge case  - last one
+            except ValueError as e:
+                print(e)
+                print("error - category_images:", category_images)
+            curr_y += 1
+            lang_dict[alphabet][1] = curr_y - 1
+    y = np.vstack(y)
+    X = np.stack(X)
+    return X,y,lang_dict
 
 X,y,c=loadimgs(basepath)
 
-print(lang_dict)
-plt.show()
-print(X.shape)
-print(X.shape)
+#pickle the arrays
 Xval,yval,cval = loadimgs(valpath)
-print(Xval.shape)
 with open("train.pickle", "wb") as f:
 	pickle.dump((X,y,c),f)
 
